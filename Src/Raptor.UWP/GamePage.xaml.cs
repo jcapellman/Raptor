8703df23b9c715f87eb1d51ef168e4dfe772aa68
Jarred Capellman <jcapellman@hotmail.com>
2016-08-06 23:28:56 -0400

    Added UWP Port

---
+++ Src/Raptor.UWP/GamePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using Raptor.GameEngine;
+
+namespace Raptor.UWP {
+    public sealed partial class GamePage : Page {
+        readonly MainGame _game;
+
+        public GamePage() {
+            this.InitializeComponent();
+
+            var launchArguments = string.Empty;
+            _game = MonoGame.Framework.XamlGame<MainGame>.Create(launchArguments, Window.Current.CoreWindow, swapChainPanel);
+        }
+    }
+}
