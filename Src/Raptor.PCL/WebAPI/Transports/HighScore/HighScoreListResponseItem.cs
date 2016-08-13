863ae89941d3f3f50798b9e289ac11ffb97c6513
Jarred Capellman <jcapellman@hotmail.com>
2016-08-12 20:47:06 -0400

    Added HighScoreListResponseItem

---
+++ Src/Raptor.PCL/WebAPI/Transports/HighScore/HighScoreListResponseItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace Raptor.PCL.WebAPI.Transports.HighScore {
+    [DataContract]
+    public class HighScoreListResponseItem {
+        [DataMember]
+        public string Username { get; set; }
+
+        [DataMember]
+        public int HighScore { get; set; }
+    }
+}
