f6986d9023f06f9a7b780a2a68926ee6119f686a
Jarred Capellman <jcapellman@hotmail.com>
2016-08-12 20:47:57 -0400

    Initial work on the Caching Attribute

---
+++ Src/Raptor.WebAPI/CustomAttributes/Cachable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+using Raptor.PCL.Enums;
+using Raptor.WebAPI.Helpers;
+
+namespace Raptor.WebAPI.CustomAttributes {
+    public class Cachable : ActionFilterAttribute {
+        private readonly WEBAPI_REQUESTS _requestEnum;
+
+        private MemoryCacheHelper _memoryCache;
+
+        public Cachable(WEBAPI_REQUESTS request, object optionalArg = null) {
+            _requestEnum = request;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context) {
+            if (!_memoryCache.ContainsKey(_requestEnum)) {
+                return;
+            }
+
+            // todo handle returning the cache item
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
