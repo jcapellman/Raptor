7720bacf08056afc25754ffaf495571bfebb97d6
Jarred Capellman <jcapellman@hotmail.com>
2016-08-18 21:23:53 -0400

    Added inital work on the Level Creation

---
+++ Src/Raptor.WebAPI.BusinessLayer/Managers/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using Raptor.PCL.Common;
+using Raptor.PCL.WebAPI.Transports.Levels;
+
+using Raptor.WebAPI.BusinessLayer.Settings;
+
+namespace Raptor.WebAPI.BusinessLayer.Managers {
+    public class LevelManager : BaseManager {
+        public LevelManager(GlobalSettings gSettings) : base(gSettings) { }
+
+        public ReturnSet<bool> CreateUpdate(LevelCreationRequestItem requestItem) {
+            return new ReturnSet<bool>(true);
+        } 
+    }
+}
---
+++ Src/Raptor.WebAPI/Controllers/LevelsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+using Raptor.PCL.Common;
+using Raptor.PCL.Enums;
+using Raptor.PCL.WebAPI.Transports.Levels;
+
+using Raptor.WebAPI.BusinessLayer.Managers;
+using Raptor.WebAPI.BusinessLayer.Settings;
+
+namespace Raptor.WebAPI.Controllers {
+    public class LevelsController : BaseController {
+        public LevelsController(GlobalSettings globalSettings, IMemoryCache memoryCache) : base(globalSettings, memoryCache) { }
+
+        public ReturnSet<bool> POST(LevelCreationRequestItem requestItem)
+            =>
+                ReturnHandler(new LevelManager(MANAGER_CONTAINER.GSetings).CreateUpdate(requestItem),
+                    WEBAPI_REQUESTS.LEVELCREATION_UPDATE);
+    }
+}
