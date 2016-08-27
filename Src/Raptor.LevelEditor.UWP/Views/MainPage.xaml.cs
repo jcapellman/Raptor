e4ddbe7ad79385baf14fbab0701a7d2756fb6cb8
Jarred Capellman <jcapellman@hotmail.com>
2016-08-26 22:15:43 -0400

    Added UWP Toolkit and HamburgerMenuControl to the LevelEditor

--- Src/Raptor.LevelEditor.UWP/Views/MainPage.xaml.cs
+++ Src/Raptor.LevelEditor.UWP/Views/MainPage.xaml.cs
@@ -1,13 +1,36 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using System.Collections.Generic;
+
+using Windows.UI.Xaml.Controls;
 
 using Raptor.LevelEditor.UWP.ViewModels;
 
 namespace Raptor.LevelEditor.UWP.Views {
-    public sealed partial class MainPage : Page {
+    public class MenuItem {
+        public Symbol Icon { get; set; }
+
+        public string Name { get; set; }
+
+        public Type PageType { get; set; }
+
+        public static List<MenuItem> GetMainItems() {
+            var items = new List<MenuItem>();
+
+            items.Add(new MenuItem() { Icon = Symbol.Contact, Name = "Your Levels", PageType = typeof(Views.MainPage) });
+            items.Add(new MenuItem() { Icon = Symbol.World, Name = "Top Rated Levels", PageType = typeof(Views.MainPage) });
+            items.Add(new MenuItem() { Icon = Symbol.Setting, Name = "Settings", PageType = typeof(Views.MainPage) });
+
+            return items;
+        }        
+    }
+
+    public sealed partial class MainPage : Page {        
         private MainPageViewModel viewModel => (MainPageViewModel) DataContext;
 
         public MainPage() {
             this.InitializeComponent();
+
+            HamburgerMenuControl.ItemsSource = MenuItem.GetMainItems();
 
             DataContext = new MainPageViewModel();
         }
