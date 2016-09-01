919c084dfaa34e192712439aa915f1f8e31c02d7
Jarred Capellman <jcapellman@hotmail.com>
2016-08-31 22:37:42 -0400

    Added initial work on Handler Wrappers for the Level Creation Browser

---
+++ Src/Raptor.PCL/WebAPI/Handlers/LevelCreationBrowserHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Raptor.PCL.Common;
+
+using Raptor.PCL.WebAPI.Common;
+using Raptor.PCL.WebAPI.Transports.LevelCreationBrowser;
+
+namespace Raptor.PCL.WebAPI.Handlers {
+    public class LevelCreationBrowserHandler : BaseHandler {
+        protected override string BaseControllerName() => "LevelCreationBrowser";
+
+        public async Task<ReturnSet<List<LevelCreationBrowserResponseItem>>> GetLevelListing()
+            => await GetAsync<ReturnSet<List<LevelCreationBrowserResponseItem>>>();
+
+        public LevelCreationBrowserHandler(HandlerWrapperItem wrapperItem) : base(wrapperItem) { }
+    }
+}
---
+++ Src/Raptor.PCL/WebAPI/Transports/LevelCreationBrowser/LevelCreationBrowserResponseItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace Raptor.PCL.WebAPI.Transports.LevelCreationBrowser {
+    [DataContract]
+    public class LevelCreationBrowserResponseItem {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int NumberDownloads { get; set; }
+    }
+}
