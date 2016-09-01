3bc822cb834e4ed1ecf0c6551aeeb80ae05bf1db
Jarred Capellman <jcapellman@hotmail.com>
2016-09-01 16:32:25 -0400

    Added LevelCreationBrowser Controlle rnad Manager Implementation

---
+++ Src/Raptor.WebAPI.BusinessLayer/Managers/LevelCreationBrowserManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Raptor.PCL.Common;
+using Raptor.PCL.WebAPI.Transports.LevelCreationBrowser;
+
+using Raptor.WebAPI.BusinessLayer.Settings;
+using Raptor.WebAPI.DataLayer.Entities;
+
+namespace Raptor.WebAPI.BusinessLayer.Managers {
+    public class LevelCreationBrowserManager : BaseManager {
+        public LevelCreationBrowserManager(GlobalSettings gSettings) : base(gSettings) { }
+
+        public ReturnSet<List<LevelCreationBrowserResponseItem>> GetLevelsListing() {
+            try {
+                using (var eFactory = new EntityFactory(DatabaseConnection)) {
+                    var result = eFactory.Levels.Where(a => a.Active).ToList();
+
+                    var response = result.Select(a => new LevelCreationBrowserResponseItem {
+                        Name = a.LevelName,
+                        NumberDownloads = 0
+                    }).ToList();
+
+                    return new ReturnSet<List<LevelCreationBrowserResponseItem>>(response);
+                }
+            } catch (Exception ex) {
+                return new ReturnSet<List<LevelCreationBrowserResponseItem>>(ex);
+            }
+        }
+    }
+}
---
+++ Src/Raptor.WebAPI/Controllers/LevelCreationBrowserController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.Extensions.Caching.Memory;
+
+using Raptor.PCL.Common;
+using Raptor.PCL.Enums;
+using Raptor.PCL.WebAPI.Transports.LevelCreationBrowser;
+using Raptor.WebAPI.BusinessLayer.Managers;
+using Raptor.WebAPI.BusinessLayer.Settings;
+
+namespace Raptor.WebAPI.Controllers {
+    public class LevelCreationBrowserController : BaseController {
+        public LevelCreationBrowserController(GlobalSettings globalSettings, IMemoryCache memoryCache) : base(globalSettings, memoryCache) { }
+
+        public ReturnSet<List<LevelCreationBrowserResponseItem>> GET()
+            =>
+                ReturnHandler(new LevelCreationBrowserManager(_globalSettings).GetLevelsListing(),
+                    WEBAPI_REQUESTS.LEVELCREATION_BROWSER_GET_LISTING);
+    }
+}
