cbbdbc6e4ef54080e1dcf15011e53afb0a8f8f0a
Jarred Capellman <jcapellman@hotmail.com>
2016-09-05 07:07:36 -0700

    Added Network Connectivity check Added Top Bar for network connectivity and user login

--- Src/Raptor.LevelEditor.UWP/App.xaml.cs
+++ Src/Raptor.LevelEditor.UWP/App.xaml.cs
@@ -1,50 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Networking.Connectivity;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
 using Raptor.LevelEditor.UWP.Views;
 
 namespace Raptor.LevelEditor.UWP {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     sealed partial class App : Application {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
+        public bool HasInternet {
+            get {
+                var connection = NetworkInformation.GetInternetConnectionProfile();
+
+                return connection?.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess;
+            }
+        }
+
         public App() {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e) {
-#if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached) {
-                this.DebugSettings.EnableFrameRateCounter = true;
-            }
-#endif
             Frame rootFrame = Window.Current.Content as Frame;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
             if (rootFrame == null) {
                 // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
